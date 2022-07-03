68f0f91d58105dade2f12ae9a1a35bdc4a1cc908
hoangvu5735 <hoangvu5735@gmail,com>
2022-07-03 21:07:41 +0700

    k

---
+++ SS7/c.js
@@ -0,0 +1,17 @@
+var smartPhones = [ 
+    { name: 'iphone', price: 649 }, 
+    { name: 'Galaxy S6', price: 576 }, 
+    { name: 'Galaxy Note 5', price: 489 } 
+];
+
+function foo(x,y,z) {
+    console.log( x, y, z );
+}
+function foo () {
+    alert(‘Hello’)
+}
+function foo (a,b){
+    let m = a+b*100
+    return m
+}
+    
