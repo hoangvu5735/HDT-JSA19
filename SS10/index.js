05dfb99eb62d5060a877186ad7f301d9cf26cd6f
hoangvu5735 <hoangvu5735@gmail,com>
2022-07-10 22:23:14 +0700

    as

---
+++ SS10/index.js
@@ -0,0 +1,13 @@
+const POKEMONAPI = "https://pokeapi.co/api/v2/pokemon/"
+const POKEMONNAME = ["accelgor","amaura","basculegion","bellsprout","blaziken","landorus","pupitar","overqwil","noivern"]
+
+for (let index = 0; index < array.length; index++) {
+console.log
+    fetch(url)
+    .then((result) => result.json())
+    .then((data) => {
+        const abilityUrl=data.abilities[0].ability.url
+    });
+
+
+}
--- SS7/c.js
+++ SS7/c.js
@@ -1,17 +1,59 @@
-var smartPhones = [ 
-    { name: 'iphone', price: 649 }, 
-    { name: 'Galaxy S6', price: 576 }, 
-    { name: 'Galaxy Note 5', price: 489 } 
+var smartPhones = [{
+        name: 'iphone',
+        price: 649
+    },
+    {
+        name: 'Galaxy S6',
+        price: 576
+    },
+    {
+        name: 'Galaxy Note 5',
+        price: 489
+    }
 ];
 
-function foo(x,y,z) {
-    console.log( x, y, z );
+function getPrice() {
+    return price
 }
-function foo () {
-    alert(‘Hello’)
-}
-function foo (a,b){
-    let m = a+b*100
+
+
+ foo=>(x, y, z) 
+    console.log(x, y, z);
+
+
+foo=>() 
+    alert('Hello')
+
+
+ a, b=>
+    let m = a + b * 100
     return m
+
+
+
+function findEven(number) {
+    if (number >= 4 && number <= 20) {
+        for (let i = 0; i <= number; i++) {
+            if (i % 2 == 0) {
+                console.log(i);
+            }
+        }
+    }
 }
-    
+
+findEven(6);
+
+const h1 = document.getElementById("header");
+h1.addEventListener("mouseover", function () {
+    h1.style.backgroundColor = "orange"
+})
+
+h1.addEventListener("mouseout", function () {
+    h1.style.backgroundColor = "darkred"
+})
+
+const hour = new Date();
+const minute = new Date();
+const second = new Date();
+
+document.write(`${hour.getHours()}:${minute.getMinutes()}:${second.getSeconds()}`)
